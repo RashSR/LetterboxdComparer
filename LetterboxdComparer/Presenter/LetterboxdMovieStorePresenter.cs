6cff9a0ac3a55e60e33d71076aded8c3b30fd8b0
scr41255 <reinhold.schlager@st.oth-regensburg.de>
2026-01-22 23:46:26 +0100

    Added hyperlink jump.

--- LetterboxdComparer/Presenter/LetterboxdMovieStorePresenter.cs
+++ LetterboxdComparer/Presenter/LetterboxdMovieStorePresenter.cs
@@ -1,6 +1,9 @@
 ﻿using LetterboxdComparer.Entities;
 using LetterboxdComparer.ViewRelated;
+using System;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Windows.Input;
 
 namespace LetterboxdComparer.Presenter
 {
@@ -16,6 +19,35 @@ namespace LetterboxdComparer.Presenter
         {
             Movies = new ObservableCollection<LetterboxdMovie>(LetterboxdMovieStore.Instance.StoredMovies);
             OnPropertyChanged(nameof(Movies));
+        }
+
+        private ICommand _openUrlCommand;
+        public ICommand OpenUrlCommand
+        {
+            get
+            {
+                if(_openUrlCommand == null)
+                {
+                    _openUrlCommand = new RelayCommand(param =>
+                    {
+                        if(param is string url)
+                            HyperlinkJump(url);
+                    });
+                }
+                return _openUrlCommand;
+            }
+        }
+
+        private void HyperlinkJump(string url)
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+            }
+            catch(Exception ex)
+            {
+                throw new InvalidOperationException($"Failed to open URL: {url}", ex);
+            }
         }
     }
 }
