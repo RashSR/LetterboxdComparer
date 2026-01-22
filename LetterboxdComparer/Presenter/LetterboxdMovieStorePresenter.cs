ff1be1a86613e914083a23133be5b8f4e685d1a1
scr41255 <reinhold.schlager@st.oth-regensburg.de>
2026-01-22 23:35:30 +0100

    Shows store entries.

--- LetterboxdComparer/Presenter/LetterboxdMovieStorePresenter.cs
+++ LetterboxdComparer/Presenter/LetterboxdMovieStorePresenter.cs
@@ -6,17 +6,15 @@ namespace LetterboxdComparer.Presenter
 {
     public class LetterboxdMovieStorePresenter : Notifier, IActivatable
     {
-        public ObservableCollection<LetterboxdMovie> Movies { get; }
+        public ObservableCollection<LetterboxdMovie> Movies { get; set; }
         public LetterboxdMovieStorePresenter()
         {
             PresenterCollection.Instance.Add(AppView.MovieStore, this);
-            Movies = new ObservableCollection<LetterboxdMovie>(LetterboxdMovieStore.Instance.StoredMovies);
         }
 
         public void OnActivated()
         {
-            LetterboxdMovie lm = new LetterboxdMovie("Example Movie", 2024, "example-uuid");
-            Movies.Add(lm);
+            Movies = new ObservableCollection<LetterboxdMovie>(LetterboxdMovieStore.Instance.StoredMovies);
             OnPropertyChanged(nameof(Movies));
         }
     }
