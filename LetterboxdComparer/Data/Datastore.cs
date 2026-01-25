77e07be9ce9ce01eb207efada8e16b36f658f4e7
scr41255 <reinhold.schlager@st.oth-regensburg.de>
2026-01-25 22:56:51 +0100

    Started basic implementation of datastore.

--- LetterboxdComparer/Data/Datastore.cs
+++ LetterboxdComparer/Data/Datastore.cs
@@ -1,5 +1,6 @@
-﻿using LetterboxdComparer.ViewRelated;
+﻿using LetterboxdComparer.Entities;
 using System;
+using System.Collections.Generic;
 
 namespace LetterboxdComparer.Data
 {
@@ -9,6 +10,89 @@ namespace LetterboxdComparer.Data
         private static readonly Lazy<Datastore> _instance = new(() => new Datastore());
         public static Datastore Instance => _instance.Value;
         private Datastore() { }
+
+        #endregion
+
+        #region Fields
+        private readonly Dictionary<Type, List<BaseEntity>> _cachedEntities = [];
+
+        #endregion
+        #region Methods
+
+        #region Add
+        public void StoreEntity<T>(T entity) where T : BaseEntity
+        {
+            Type type = typeof(T);
+            if (!_cachedEntities.TryGetValue(type, out List<BaseEntity>? value))
+            {
+                value = new List<BaseEntity>();
+                _cachedEntities[type] = value;
+            }
+            value.Add(entity);
+        }
+
+        public void StoreEntities<T>(List<T> entities) where T : BaseEntity
+        {
+            Type type = typeof(T);
+            if (!_cachedEntities.TryGetValue(typeof(T), out List<BaseEntity>? value))
+            {
+                value = new List<BaseEntity>();
+                _cachedEntities[type] = value;
+            }
+
+            value.AddRange(entities);
+        }
+
+        #endregion
+
+        #region Get
+        public T? GetEntity<T>(int id) where T : BaseEntity
+        {
+            Type type = typeof(T);
+            if (_cachedEntities.TryGetValue(type, out List<BaseEntity>? value))
+            {
+                foreach(BaseEntity entity in value)
+                {
+                    if (entity.Id == id)
+                        return (T)entity;
+                }
+            }
+
+            return null;
+        }
+        public List<T> GetEntities<T>() where T : BaseEntity
+        {
+            Type type = typeof(T);
+            if (_cachedEntities.TryGetValue(type, out List<BaseEntity>? value))
+            {
+                List<T> typedList = value.ConvertAll(item => (T)item);
+                return typedList;
+            }
+
+            return [];
+        }
+
+        #endregion
+
+        #region Remove
+        public void RemoveEntity<T>(T entity) where T : BaseEntity
+        {
+            Type type = typeof(T);
+            if (_cachedEntities.TryGetValue(type, out List<BaseEntity>? value))
+                value.Remove(entity);
+        }
+
+        public void RemoveEntities<T>(List<T> entities) where T : BaseEntity
+        {
+            Type type = typeof(T);
+            if (_cachedEntities.TryGetValue(type, out List<BaseEntity>? value))
+            {
+                foreach(T entity in entities)
+                    value.Remove(entity);
+            }
+        }
+
+        #endregion
 
         #endregion
     }
--- LetterboxdComparer/Presenter/ProfilePresenter.cs
+++ LetterboxdComparer/Presenter/ProfilePresenter.cs
@@ -1,6 +1,8 @@
-﻿using LetterboxdComparer.Entities;
+﻿using LetterboxdComparer.Data;
+using LetterboxdComparer.Entities;
 using LetterboxdComparer.ViewRelated;
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
 namespace LetterboxdComparer.Presenter
@@ -11,15 +13,22 @@ namespace LetterboxdComparer.Presenter
         {
             PresenterCollection.Instance.Add(AppView.Profile, this);
             StoredUsers = [];
+            LetterboxdUser user = new("Rash", DateTime.Now);
+            user.Id = 1;
+            LetterboxdUser user2 = new("Rash2", DateTime.Now);
+            user2.Id = 2;
+            List<LetterboxdUser> users = [ user, user2 ];
+            Datastore.Instance.StoreEntities(users);
+            Datastore.Instance.RemoveEntity(user);
         }
 
         public ObservableCollection<LetterboxdUser> StoredUsers { get; set; }
 
         public void OnActivated()
         {
-            LetterboxdUser user = new("ExampleUser", DateTime.Now);
-            StoredUsers.Add(user);
+            List<LetterboxdUser> users = Datastore.Instance.GetEntities<LetterboxdUser>();
+            foreach(LetterboxdUser user in users)
+                StoredUsers.Add(user);
         }
     }
-
 }
